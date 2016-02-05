064a2a36729f53a54be80831eaa936607f534292
Shazil <Shazil@10.102.188.157>
2016-02-05 17:54:34 +0100

    Hey Gerri ! Check !

--- SourceCode/src/fr/shazilgerard/findmypatient/controller/main.java
+++ SourceCode/src/fr/shazilgerard/findmypatient/controller/main.java
@@ -5,7 +5,9 @@ package fr.shazilgerard.findmypatient.controller;
 
 import java.awt.EventQueue;
 
+import fr.shazilgerard.findmypatient.dao.exceptions.DaoInitializationException;
 import fr.shazilgerard.findmypatient.view.PatientOverviewView;
+import fr.shazilgerard.findmypatient.view.loginview;
 
 /**
  * @author Gerard
@@ -19,15 +21,25 @@ public class main {
 	public static void main(String[] args) {
 		
 		// Controller
+	
 		IdentityController controller = new IdentityController();
-		
+		try {
+			controller.setupDatabase("jdbc:derby://localhost:1527/PatientsDB;create=true", "root", "root");
+		} catch (DaoInitializationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
 		// Start GUI
 		System.out.println("Starting GUI");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PatientOverviewView frame = new PatientOverviewView(controller);
-					frame.setVisible(true);
+					//PatientOverviewView frame = new PatientOverviewView(controller);
+					//frame.setVisible(true);
+					
+					loginview obj = new loginview(controller);
+					
 					
 					System.out.println("Program running");
 				} catch (Exception e) {
---
+++ SourceCode/src/fr/shazilgerard/findmypatient/view/loginview.java
@@ -0,0 +1,105 @@
+package fr.shazilgerard.findmypatient.view;
+
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import fr.shazilgerard.findmypatient.controller.IdentityController;
+import fr.shazilgerard.findmypatient.dao.exceptions.DaoLoadObjectException;
+import fr.shazilgerard.findmypatient.datamodel.exceptions.NoAuthorityException;
+
+import java.io.*;
+
+
+public class loginview
+{
+	private  JFrame obj;
+	private  JTextField loginField;
+	private  JPasswordField passField;
+	
+	private  JLabel username;
+	private  JLabel password;
+	private  JTextArea pta;
+	
+	private  JButton log_in;
+	private  JButton cancel;
+	
+	private  JPanel panel;
+	private  JPanel panel1;
+	private  JPanel panel2;
+	
+	private final IdentityController identityController;
+    
+	public loginview(final IdentityController identityController)
+	{
+		this.identityController = identityController;
+		
+	    
+		
+	    JFrame.setDefaultLookAndFeelDecorated(true);
+	    obj = new JFrame("Hospital Managment System");
+		
+
+		JPanel panel  = new JPanel();
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+   
+    	
+    	
+    	JLabel label = new JLabel(new ImageIcon("./images/hospital.jpg"));
+        panel.add(label);
+		obj.add(panel,BorderLayout.NORTH);
+	
+		
+		
+		panel1.setLayout(new GridLayout(3,2));
+		username = new JLabel(" **** Username **** :");
+		password = new JLabel(" **** Password **** :");
+		loginField = new JTextField("");
+		passField = new JPasswordField("");
+		panel1.add(username);
+		panel1.add(loginField);
+		panel1.add(password);
+		panel1.add(passField);
+		log_in = new JButton("Login");
+		cancel = new JButton("Cancel");
+		panel1.add(log_in);
+		panel1.add(cancel);
+		obj.add(panel1,BorderLayout.CENTER);
+		
+	
+	
+		
+		
+		log_in.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent e)
+			{
+				
+			try {
+				identityController.getUserAuthority().login(loginField.toString(), passField.toString());
+			} catch (NoAuthorityException | DaoLoadObjectException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+				
+				
+			}		
+		});
+		
+		
+		cancel.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent sig)
+			{
+			
+		
+			}});
+
+	
+	obj.setSize(550,300);		
+	obj.setVisible(true);			
+	obj.setDefaultCloseOperation(obj.EXIT_ON_CLOSE);
+	
+	}}
